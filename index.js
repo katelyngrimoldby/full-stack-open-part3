2471f5787363993ad0af68a0eda741fd6957a2e4
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-22 11:55:37 -0700

    3.11

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 

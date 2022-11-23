34f11d0e503704b74461d1d80f93ae8671d9971b
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-23 13:58:38 -0700

    3.22

--- index.js
+++ index.js
@@ -1,118 +1,106 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const Entry = require('./models/entry')
-const { response } = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const Entry = require('./models/entry');
+const app = express();
 
-app.use(express.json())
-app.use(express.static('dist'))
-app.use(cors())
-
-const getRandomNumber = (range) => {
-  const number =  Math.floor(Math.random() * range)
-
-  if(entries.find(entry => entry.id === number)) {
-    return getRandomNumber(range)
-  } else {
-    return number
-  }
-}
+app.use(express.json());
+app.use(express.static('dist'));
+app.use(cors());
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({error: 'unknown endpoint'})
-}
+  res.status(404).send({ error: 'unknown endpoint' });
+};
 
 const errorHandler = (error, req, res, next) => {
   if(error.name === 'CastError') {
-    return res.status(400).json({error: 'malformatted id'})
+    return res.status(400).json({ error: 'malformatted id' });
   }
   if(error.name === 'ValidationError') {
-    return res.status(400).json({error: error.message})
+    return res.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
 app.get('/info', (req, res) => {
-  const date = new Date()
+  const date = new Date();
   Entry.find({})
     .then(entries => {
       const message = `<div>
-      <p>Phonebook has information for ${entries.length} person/people</p>
-      <p>${date.toLocaleString('en-US')}</p>
-    </div>`
-    res.send(message)
-    })
-})
+        <p>Phonebook has information for ${entries.length} person/people</p>
+        <p>${date.toLocaleString('en-US')}</p>
+      </div>`;
+      res.send(message);
+    });
+});
 
 app.get('/api/entries', (req, res) => {
-  Entry.find({}).then(entries => res.json(entries))
-})
+  Entry.find({}).then(entries => res.json(entries));
+});
 
 app.get('/api/entries/:id', (req, res, next) => {
   Entry.findById(req.params.id)
-  .then(entry => {
-    if(entry) {
-      res.json(entry)
-    } else {
-      res.status(404).end()
-    }
-  })
-  .catch(error => next(error))
-})
+    .then(entry => {
+      if(entry) {
+        res.json(entry);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
 
 app.post('/api/entries', (req, res, next) => {
-  const body = req.body
+  const body = req.body;
 
   const entry = new Entry({
     name: body.name,
     number: body.number,
-  })
+  });
 
-  Entry.find({name: body.name})
+  Entry.find({ name: body.name })
     .then(entries => {
       if(entries.length > 0) {
-        return res.status(400).json({error: `${body.name} is already in the phonebook`})
+        return res.status(400).json({ error: `${body.name} is already in the phonebook` });
       } else {
         entry.save()
-        .then(result => res.json(result))
-        .catch(error => next(error))
+          .then(result => res.json(result))
+          .catch(error => next(error));
       }
-    })
-
-  
-})
+    });
+});
 
 app.delete('/api/entries/:id', (req, res, next) => {
   Entry.findByIdAndRemove(req.params.id)
-  .then(result => {
-    if(result) {
-      res.status(204).end()
-    } else {
-      res.status(404).end()
-    }
-  })
-  .catch(error => next(error))
-})
+    .then(result => {
+      if(result) {
+        res.status(204).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
 
 app.put('/api/entries/:id', (req, res, next) => {
-  const body = req.body
+  const body = req.body;
 
   const entry = {
     name: body.name,
     number: body.number
-  }
+  };
 
-  Entry.findByIdAndUpdate(req.params.id, entry, {new: true, runValidators: true, context: 'query'})
-  .then(entry => res.json(entry))
-  .catch(error => next(error))
-})
+  Entry.findByIdAndUpdate(req.params.id, entry, { new: true, runValidators: true, context: 'query' })
+    .then(entry => res.json(entry))
+    .catch(error => next(error));
+});
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
-const PORT = process.env.PORT
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/entry.js
+++ models/entry.js
@@ -1,42 +1,43 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-const url = process.env.MONGO_URI
+// eslint-disable-next-line no-undef
+const url = process.env.MONGO_URI;
 
-console.log('connecting to MongoDB')
+console.log('connecting to MongoDB');
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    console.log('connected to MongoDB');
   })
   .catch(error => {
-    console.log(`Error connecting to MongoDB: ${error.message}`)
-  })
+    console.log(`Error connecting to MongoDB: ${error.message}`);
+  });
 
-  const entrySchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: [3, 'Name must be at least three characters long'],
-      required: [true, 'Name is required']
-    },
-    number: {
-      type: String,
-      required: [true, 'Number is required'],
-      validate: {
-        validator: function(v) {
-          return /\d{2,3}-\d{6,}/.test(v)
-        },
-        message: props => `${props.value} is not a valid phone number`
-      }
+const entrySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: [3, 'Name must be at least three characters long'],
+    required: [true, 'Name is required']
+  },
+  number: {
+    type: String,
+    required: [true, 'Number is required'],
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number`
     }
-  })
+  }
+});
 
-  entrySchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-  
-module.exports = mongoose.model('Entry', entrySchema)
+entrySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Entry', entrySchema);

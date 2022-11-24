f22786a40a6495e056afc1b261c1d8845ae7d687
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-23 20:02:18 -0700

    4.5

--- part4/utils/listHelper.js
+++ part4/utils/listHelper.js
@@ -15,7 +15,28 @@ const totalLikes = (blogs) => {
   }
 };
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  } else if (blogs.length === 1) {
+    return {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes
+    };
+  } else {
+    const blog = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current);
+
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    };
+  }
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 };

08b06829d1fe3a8a8e0efb85efa188c7119f89e1
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-23 19:25:17 -0700

    4.4

--- part4/utils/listHelper.js
+++ part4/utils/listHelper.js
@@ -3,6 +3,19 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  } else if (blogs.length === 1) {
+    return blogs[0].likes;
+  } else {
+    const likes = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+
+    return likes;
+  }
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };

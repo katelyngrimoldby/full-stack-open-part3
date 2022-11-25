734370619526045845192c8df2e04e6d91f17a94
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-11-25 11:04:14 -0700

    4.18

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -6,7 +6,10 @@ const MONGO_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGO_URI
   : process.env.MONGO_URI;
 
+const SECRET = process.env.SECRET;
+
 module.exports = {
   MONGO_URI,
-  PORT
+  PORT,
+  SECRET
 };
